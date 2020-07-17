8bc535769de6322ad27420a1acf63abc9c8be9ba
Nick <pihluk@gmail.com>
2020-07-17 00:31:27 -0700

    chanmge

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-''
+"use strict";
 
 const numberOfFilm = +prompt('Сколько фильмов вы прсмотрели', '');
 
 const personalMovirDB = {
-    count: {},
+    count: numberOfFilm,
     movies: {},
     actors: {},
     genres: {},
@@ -11,12 +11,33 @@ const personalMovirDB = {
 
 };
 
-const a = prompt('Which cind of film you see last time', '' ),
-      b = prompt('Do you like it?', ''),
-      c = prompt('Which cind of film you see last time', '' ),
-      d = prompt('Do you like it?', '');  
-      
-personalMovirDB.movies[a] = b;     
-personalMovirDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Which cind of film you see last time', '' ),
+          b = prompt('Do you like it?', '');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovirDB.movies[a] = b;  
+        console.log('OK');
+    }else{
+        console.log("error");
+        i--;
+    }      
+
+        
+}
+
+if (personalMovirDB.count < 10) {
+    console.log("Very less");
+
+} else if (personalMovirDB.count >= 10 && personalMovirDB.count < 30 ) {
+    console.log('Very good');
+} else if (personalMovirDB.count >= 30) {
+    console.log('You are cinimamen');
+} else {
+    console.log("fail");
+}
+
 
 console.log(personalMovirDB);
+
+
