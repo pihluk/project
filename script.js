aa9ad04d10ced7cc4106564a1ddb7f757893f874
Nick <pihluk@gmail.com>
2020-07-19 20:37:21 -0700

    practick 5

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 "use strict";
 
-const numberOfFilm = +prompt('Сколько фильмов вы прсмотрели', '');
+let numberOfFilm;
+
+function start () {
+    numberOfFilm = +prompt('Сколько фильмов вы прсмотрели', '');
+
+    while (numberOfFilm == '' || numberOfFilm == null || isNaN(numberOfFilm)){
+        numberOfFilm = +prompt('Сколько фильмов вы прсмотрели', '');
+    }
+
+}
+
+start();
 
 const personalMovirDB = {
     count: numberOfFilm,
@@ -11,33 +22,58 @@ const personalMovirDB = {
 
 };
 
-for (let i = 0; i < 2; i++) {
-    const a = prompt('Which cind of film you see last time', '' ),
-          b = prompt('Do you like it?', '');
+function rememberMyFilms (){
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Which cind of film you see last time', '' ),
+              b = prompt('Do you like it?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovirDB.movies[a] = b;  
+            console.log('OK');
+        }else{
+            console.log("error");
+            i--;
+        }      
+    
+    }  
+}           
+rememberMyFilms();
 
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovirDB.movies[a] = b;  
-        console.log('OK');
-    }else{
-        console.log("error");
-        i--;
-    }      
-
-        
+function detectPersonalLevel(){
+    if (personalMovirDB.count < 10) {
+        console.log("Very less");
+    
+    } else if (personalMovirDB.count >= 10 && personalMovirDB.count < 30 ) {
+        console.log('Very good');
+    } else if (personalMovirDB.count >= 30) {
+        console.log('You are cinimamen');
+    } else {
+        console.log("fail");
+    }
 }
+detectPersonalLevel();
 
-if (personalMovirDB.count < 10) {
-    console.log("Very less");
+function shoMyDB (hidden) {
+    if (!hidden){
+        console.log(personalMovirDB);
 
-} else if (personalMovirDB.count >= 10 && personalMovirDB.count < 30 ) {
-    console.log('Very good');
-} else if (personalMovirDB.count >= 30) {
-    console.log('You are cinimamen');
-} else {
-    console.log("fail");
+    }
 }
-
+shoMyDB(personalMovirDB);
 
 console.log(personalMovirDB);
 
+function showFirstMessage(){
+    console.log('Helllo, world');
+     
+}
 
+
+showFirstMessage();
+
+function writeYoreGenerse () {
+    for (let i = 1; i <= 3; i++){
+        personalMovirDB.genres[i - 1] = prompt(`Youre lovle film number ${i}`); 
+    }
+}
+writeYoreGenerse();
