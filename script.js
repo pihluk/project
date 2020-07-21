6d50e7378d98cbe3d787f5f97e370aaffaa7678f
Nick <pihluk@gmail.com>
2020-07-20 21:04:05 -0700

    change code

--- script.js
+++ script.js
@@ -1,79 +1,84 @@
-"use strict";
+'use strict';
 
-let numberOfFilm;
-
-function start () {
-    numberOfFilm = +prompt('Сколько фильмов вы прсмотрели', '');
-
-    while (numberOfFilm == '' || numberOfFilm == null || isNaN(numberOfFilm)){
-        numberOfFilm = +prompt('Сколько фильмов вы прсмотрели', '');
-    }
-
-}
-
-start();
 
 const personalMovirDB = {
-    count: numberOfFilm,
+    count: 0,
     movies: {},
     actors: {},
     genres: {},
-    privet: false
-
-};
-
-function rememberMyFilms (){
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Which cind of film you see last time', '' ),
-              b = prompt('Do you like it?', '');
+    privet: false,
+    start: function () {
+        personalMovirDB.count = +prompt('Сколько фильмов вы прсмотрели', '');
     
-        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-            personalMovirDB.movies[a] = b;  
-            console.log('OK');
-        }else{
-            console.log("error");
-            i--;
-        }      
-    
-    }  
-}           
-rememberMyFilms();
+        while (personalMovirDB.count == '' || personalMovirDB.count == null || isNaN(personalMovirDB.count)){
+            personalMovirDB.count = +prompt('Сколько фильмов вы прсмотрели', '');
+        }
+    },
 
-function detectPersonalLevel(){
-    if (personalMovirDB.count < 10) {
-        console.log("Very less");
-    
-    } else if (personalMovirDB.count >= 10 && personalMovirDB.count < 30 ) {
-        console.log('Very good');
-    } else if (personalMovirDB.count >= 30) {
-        console.log('You are cinimamen');
-    } else {
-        console.log("fail");
-    }
-}
-detectPersonalLevel();
+    rememberMyFilms: function (){
+        for (let i = 0; i < 2; i++) {
+            const a = prompt('Which cind of film you see last time', '' ),
+                  b = prompt('Do you like it?', '');
+        
+            if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+                personalMovirDB.movies[a] = b;  
+                console.log('OK');
+            }else{
+                console.log("error");
+                i--;
+            }      
+        
+        }
+    },
+        detectPersonalLevel: function(){
+            if (personalMovirDB.count < 10) {
+                console.log("Very less");
+            
+            } else if (personalMovirDB.count >= 10 && personalMovirDB.count < 30 ) {
+                console.log('Very good');
+            } else if (personalMovirDB.count >= 30) {
+                console.log('You are cinimamen');
+            } else {
+                console.log("fail");
+            }
+        },      
+        shoMyDB:function (hidden) {
+            if (!hidden){
+                console.log(personalMovirDB);
+        
+            }
+        },
 
-function shoMyDB (hidden) {
-    if (!hidden){
-        console.log(personalMovirDB);
+        toggleVisibleMyDB: function(){
+            if(personalMovirDB.privet){
+                personalMovirDB.privet = false;
+            } else{
+                personalMovirDB.privet = true;
 
-    }
-}
-shoMyDB(personalMovirDB);
-
-console.log(personalMovirDB);
-
-function showFirstMessage(){
-    console.log('Helllo, world');
+            }
      
-}
+        },
+
+        writeYoreGenerse:function () {
+            for (let i = 1; i <= 2; i++){
+                let genres = prompt(`Youres lovle film `).toLocaleLowerCase();
+
+                if (genres ==='' || genres == null){
+                    console.log('pls complete text');
+                    i--;
+                } else{
+                personalMovirDB.genres = genres.split(`,` );
+                personalMovirDB.genres.sort();
+             }
+
+            }
+           personalMovirDB.genres.forEach((item, i) => {
+                console.log(`Любимый жанр ${i + 1} - это ${item}`);
+            });
+        }
+    };
 
 
-showFirstMessage();
 
-function writeYoreGenerse () {
-    for (let i = 1; i <= 3; i++){
-        personalMovirDB.genres[i - 1] = prompt(`Youre lovle film number ${i}`); 
-    }
-}
-writeYoreGenerse();
+
+
